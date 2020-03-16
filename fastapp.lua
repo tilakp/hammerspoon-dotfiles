1298fc4be042f4b8ae2aa1f967cfe71bcd1b08a4
Tilak Patel <tilak[dot]patel[at]gmail[dot]com>
2020-03-15 18:31:19 -0700

    Adding outlook fast launch shortcut

--- fastapp.lua
+++ fastapp.lua
@@ -2,6 +2,10 @@ hs.hotkey.bind(hyper, "1", function()
   hs.application.launchOrFocus("Microsoft OneNote")
 end)
 
+hs.hotkey.bind(hyper, "0", function()
+  hs.application.launchOrFocus("Microsoft Outlook")
+end)
+
 hs.hotkey.bind(hyper, "c", function()
   hs.application.launchOrFocus("Google Chrome")
 end)
